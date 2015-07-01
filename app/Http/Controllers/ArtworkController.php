b128b99ff3e891e9237e8d79aa1596461d6b153e
Dennis Kievits <denniskievits@Denniss-MacBook-Pro.local>
2015-07-01 20:16:20 +0200

    bla

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -26,6 +26,7 @@ class ArtworkController extends Controller {
 	 */
 	public function index()
 	{
+		// Show the gallery index
 		return Redirect::action('PagesController@gallery');
 	}
 	
@@ -36,17 +37,22 @@ class ArtworkController extends Controller {
 	 */
 	public function create()
 	{
+		// Is the user a moderator or admin?
 		if (Auth::check() && Auth::user()->hasOnePrivelege(['Moderator', 'Administrator'])) 
 		{
+			// Show the super create
 			return View::make('artworks/create');
 		} 
+		// Is the user a student?
 		else if (Auth::check() && Auth::user()->hasOnePrivelege(['Student'])) 
 		{
+			// Show the student version of the create
 			return View::make('artworks/studentCreate');
 		} 
 		else
 		{
-			return View::make('errors/401'); // Unauthorized
+			// Show the unauthorized page
+			return View::make('errors/' . HttpCode::Unauthorized); // Unauthorized
 		}
 	}
 
@@ -57,54 +63,80 @@ class ArtworkController extends Controller {
 	 */
 	public function store()
 	{
+		// Is the user a student or moderator or admin?
 		if (Auth::check() && Auth::user()->hasOnePrivelege(['Student', 'Moderator', 'Administrator'])) 
 		{
+			// Get all post data
 			$input = Input::all();
+			// Create an artwork
 			$artwork = new Artwork();
+			// Set the id one higher than the amount of artworks
 			$artwork->id = Artwork::count() + 1;
+			// Set the title to the input
 			$artwork->title = Input::get('title');
+			// Set the description to a trimmed version of the input (removing whitespaces)
 			$artwork->description = trim(Input::get('description'));
+			// Set the reserved to 0
 			$artwork->reserved = 0;
+			// get the tags [test,mark] <-- format and split them by a , to an array
 			$tags = explode(',', $input['tags']);
 
+			// Is the user an moderator or admin
 			if (Auth::user()->hasOnePrivelege(['Moderator', 'Administrator'])) 
 			{
+				// If the publish checkbox was checked set publish to true
 				$artwork->state = Input::get('publish') == "true" ? 0 : 1;
 			} 
 			else
 			{
+				// Else make it archived
 				$artwork->state = 1;
 			}
 
+			// Create a slug from the title
+			// replace all spaces by a -
 			$slug = strtolower(implode('-', explode(' ', Input::get('title'))));
+			// replace all ?, /, \\ by nothing
 			$slug = str_replace('?','', $slug);
 			$slug = str_replace('/','',$slug );
 			$slug = str_replace('\\','',$slug );
 
-			if (Artwork::where('slug', $slug)->first()) {
+			// check if the slug already exist.
+			if (Artwork::where('slug', $slug)->first()) 
+			{
+				// Tell the user this title is already being used
 				return Response::json([0 => 'Deze titel is al gebruikt bij een ander kunstwerk.'], HttpCode::Conflict);
 			}
 
+			// Set the slug to the slug we created
 			$artwork->slug = $slug;
 
+			// Retrieve the image
 			$image = Image::make(Input::get('image-data-url'));
 			
+			// Get the image extension ex: png, jpg
 			$imageExtension = substr($image->mime(), 6);
 
+			// set the image file to images/artworks/artwork number.extension
 			$artwork->file = 'images/artworks/' . $artwork->id . '.' . $imageExtension;
 			/**
 			 * @todo add middleware to check if logged in.
 			 */
 			$artwork->user_id = Auth::user()->id;
 
-			$image->save('images/artworks/' . $artwork->id . '.' . $imageExtension);
+			// save it at the files place
+			$image->save($artwork->file);
 
+			// tag the artwork with all the tags
 			foreach ($tags as $tag) 
 			{
 				$artwork->tag($tag);
 			}
+
+			// save the artwork data in the database
 			$artwork->save();
 
+			// Success
 			return Response::json([
 				0 => 'Het kunstwerk is aangemaakt klik <a href="/artworks/' . $artwork->slug . '">hier</a> om het the bekijken',
 				1 => 'of klik <a href="/gallery"> hier </a> om terug te keren naar de gallerij'
@@ -112,6 +144,7 @@ class ArtworkController extends Controller {
 		} 
 		else 
 		{
+			// Unauthorized error
 			return Response::json([
 				0 => 'Je bent niet geautoriseerd.'
 			], 401);
@@ -126,17 +159,22 @@ class ArtworkController extends Controller {
 	 */
 	public function show($slug)
 	{
+		// get the artwork by the slug
 		$artwork = Artwork::whereSlug($slug)->first();
 
-		$tagArray = $artwork->tagNames();
-
+		// Does the artwork exist?
 		if ($artwork) 
 		{
+			// get the tags
+			$tagArray = $artwork->tagNames();
+
+			// Show the view
 			return View::make('artworks/show')->with(compact('artwork','tagArray'));
 		}
 		else 
 		{
-			return View::make('errors/404');
+			// Show a not found page
+			return View::make('errors/' . HttpCode::NotFound);
 		}
 	}
 
@@ -148,14 +186,19 @@ class ArtworkController extends Controller {
 	 */
 	public function edit($slug)
 	{
+		// get the artwork
 		$artwork = Artwork::where('slug', $slug)->first();
+
+		// Does the artwork exist?
 		if ($artwork) 
 		{
+			// Show the view
 			return View::make('artworks/edit', compact('artwork'));
 		} 
 		else 
 		{
-			throw new \Exception('Kunstwerk is niet gevonden in de database.');
+			// Show the not found page
+			return View::make('errors/' . HttpCode::NotFound);
 		}
 	}
 
@@ -167,44 +210,60 @@ class ArtworkController extends Controller {
 	 */
 	public function update($id)
 	{
+		// Get our artwork
+		$artwork = Artwork::findOrFail($id);
+
+		// get all post data
 		$input = Input::all();
 
+		// Remove line endings from our description
 		$input['description'] = str_replace("\n", '', $input['description']); // remove line endings
 		$input['description'] = str_replace("\r", '', $input['description']); // remove line endings
 		
-		$artwork = Artwork::findOrFail($id);
-
+		// make an tag array from the string
 		$tags = explode(',', $input['tags']);
 
+		// Make the title to a slug form
 		$slug = strtolower(implode('-', explode(' ', Input::get('title'))));
 		$slug = str_replace('?','', $slug);
 		$slug = str_replace('/','',$slug );
 		$slug = str_replace('\\','',$slug );
+
+		// Set the slug
 		$artwork->slug = $slug;
 
+		// Get the image
 		$image = Image::make(Input::get('image-data-url'));
 			
+		// Get the extension ex: png, jpg
 		$imageExtension = substr($image->mime(), 6);
 
+		// Set the file
 		$artwork->file = 'images/artworks/' . $artwork->id . '.' . $imageExtension;
 		$artwork->user_id = Auth::user()->id;
 		
-		$image->save('images/artworks/' . $artwork->id . '.' . $imageExtension);
+		// Save the image
+		$image->save($artwork->file);
 
+		// Is the user a moderator or admin?
 		if (Auth::user()->hasOnePrivelege(['Moderator', 'Administrator'])) 
 		{
+			// If the checkbox is checked publish immediately
 			$artwork->state = Input::get('publish') == "true" ? 0 : 1;
 		} 
 		else 
 		{
+			// Set to state to archived
 			$artwork->state = 1;
 		}
 
+		// Tag our artwork with the tags given
 		foreach ($tags as $tag) 
 		{
 			$artwork->tag($tag);
 		}
-			
+		
+		// Save our artwork
 		$artwork->save();
 		$artwork->update($input);	
 
