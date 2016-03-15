6dac781b022f81eeffd1d7ed90e926bc73579bdf
Dion Oosthoek <dion-oosthoek@hotmail.com>
2016-03-15 10:11:28 +0100

    Zijmenu werkt nu echt zoals het hoort

--- public/js/navbar-toggle.js
+++ public/js/navbar-toggle.js
@@ -1,6 +1,16 @@
-$("#custom_navbar_toggle").on( 'click', function () {
-    $("#custom_navmenu").animate({width:'toggle'},350);
-});
-$("#custom_navbar_toggle_two").on( 'click', function () {
-      $("#custom_navmenu").animate({width:'toggle'},350);
-});
+var sidebar_nav = document.getElementById("custom_navmenu");
+var toggleState = 0;
+function menuToggleFunction() {
+	if (toggleState == 0) {
+        sidebar_nav.style.transition = "right 0.3s ease-out 0s";
+        sidebar_nav.style.right = 0;
+        toggleState = 1;
+    }
+    else {
+    	sidebar_nav.style.transition = "right 0.3s ease-out 0s";
+        sidebar_nav.style.right = '-300px';
+        toggleState = 0;
+    }
+}
+$("#custom_navbar_toggle").click(function() {menuToggleFunction()});
+$("#custom_navbar_toggle_two").click(function() {menuToggleFunction()});
