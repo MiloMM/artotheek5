1da512dd592a336a245ce417216d4958afb87b07
Dion Oosthoek <dion-oosthoek@hotmail.com>
2016-04-11 11:31:28 +0200

    Searchbar menu toggle
    als je velden toe wil voegen dan moet het in de div zetten met het ID "customSearchControlDiv"

--- public/js/navbar-toggle.js
+++ public/js/navbar-toggle.js
@@ -1,5 +1,7 @@
 var sidebar_nav = document.getElementById("custom_navmenu");
+var searchbar_nav = document.getElementById("customSearchControlDiv");
 var toggleState = 0;
+var sBarToggleState = 0;
 function menuToggleFunction() {
 	if (toggleState == 0) {
         sidebar_nav.style.transition = "right 0.3s ease-out 0s";
@@ -12,5 +14,18 @@ function menuToggleFunction() {
         toggleState = 0;
     }
 }
+function searchbarToggleFunction() {
+    if (sBarToggleState == 0) {
+        searchbar_nav.style.transition = "top 0.3s ease-out 0s";
+        searchbar_nav.style.top = '51px';
+        sBarToggleState = 1;
+    }
+    else {
+        searchbar_nav.style.transition = "top 0.3s ease-out 0s";
+        searchbar_nav.style.top = '-200px';
+        sBarToggleState = 0;
+    }
+}
 $("#custom_navbar_toggle").click(function() {menuToggleFunction()});
 $("#custom_navbar_toggle_two").click(function() {menuToggleFunction()});
+$("#customSearchControlBtn").click(function() {searchbarToggleFunction()});
