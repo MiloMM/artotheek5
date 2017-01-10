ba43b52ffd417a54cf01c71c480079af3366e052
Jelle <jelle@avant.nl>
2017-01-10 14:12:15 +0100

    Wat veranderingen.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,76 +7,79 @@
 		<a href="/users/destroy/{{ $user->id }}" class="fa fa-times fa-2x profileDeleteButton" title="Account verwijderen" onclick="return confirm('Weet u zeker dat u dit profiel en account wilt verwijderen? De kunstenaar die aan dit profiel is gekoppeld blijft bestaan.')"></a>
 	@endif
 </div>
-	@if ($user->profile_picture != '' || $user->biography != '')
-		<div class="container profileDetails">
-			<div class="col-md-12">
-				<div class="col-md-3"><img src="{{$user->profile_picture}}" class="profile_pic"></div>
-				<div class="col-md-9">{!! $user->biography !!}</div>
+	<div class="container profileDetails" style="padding: 0;">
+		<div class="col-md-10">
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Naam</b></div>
+				<div class="col-md-9">{{$user->name}}</div>
 			</div>
+			
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Lid sinds</b></div>
+				<div class="col-md-9">{{$user->created_at}}</div>
+			</div>
+			@if (Auth::check() && Auth::user()->hasOnePrivelege(['Administrator']))
+			<div class="col-md-6">
+				<div class="col-md-3"><b>E-mail</b></div>
+				<div class="col-md-9">{{$user->email}}</div>
+			</div>
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Telefoon nummer</b></div>
+				<div class="col-md-9">{{$user->telephone}}</div>
+			</div>
+			@endif
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Opleiding / Sector</b></div>
+				<div class="col-md-9">{{$user->education}}</div>
+			</div>	
+			
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Leerjaar</b></div>
+				<div class="col-md-9">{{$user->school_year}}</div>
+			</div>
+			{{--<div class="col-md-6">
+				<div class="col-md-3"><b>Overzicht werk</b></div>
+				<div class="col-md-9">{{$user->work_summary}}</div>
+			</div>
+			<div class="col-md-6">
+				<div class="col-md-3"><b>Kostenplaatje</b></div>
+				<div class="col-md-9">{{$user->price}}</div>
+			</div>--}}
+			@if (Auth::check() && Auth::user()->hasOnePrivelege(['Administrator']))
+				<div class="col-md-6">
+					<div class="col-md-3"><b>Bezorg adres</b></div>
+					<div class="col-md-9">{{$user->delivery_address}}</div>
+				</div>
+				<div class="col-md-6">
+					<div class="col-md-3"><b>Postcode</b></div>
+					<div class="col-md-9">{{$user->zip}}</div>
+				</div>
+			@endif
+			@if ($user->biography != '')
+				<div class="col-md-12 biography">{!! $user->biography !!}</div>
+			@endif
 		</div>
+		<div class="col-md-2">
+			@if ($user->profile_picture != '')
+				<img src="{{$user->profile_picture}}" class="profile_pic">
+			@else
+				<i class="no_profile_pic">Geen profielafbeelding</i>
+			@endif
+		</div>
+	</div>
+	<h4>Kunstwerken</h4>
+	@if (count($artworks) > 0)
+		@foreach ($artworks as $artwork)
+		<div class="img-box-search">
+			<a href="/artworks/{{ $artwork->slug }}">
+				<img class="img-box-image" src="/{{$artwork->file}}" />
+				<h3>{{$artwork->title}}</h3>
+			</a>
+		</div>
+		@endforeach
+	@else
+		<i>Geen</i>
 	@endif
-	<div class="container profileDetails">
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Naam</b></div>
-			<div class="col-md-9">{{$user->name}}</div>
-		</div>
-		
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Lid sinds</b></div>
-			<div class="col-md-9">{{$user->created_at}}</div>
-		</div>
-		@if (Auth::check() && Auth::user()->hasOnePrivelege(['Administrator']))
-		<div class="col-md-6">
-			<div class="col-md-3"><b>E-mail</b></div>
-			<div class="col-md-9">{{$user->email}}</div>
-		</div>
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Telefoon nummer</b></div>
-			<div class="col-md-9">{{$user->telephone}}</div>
-		</div>
-		@endif
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Opleiding / Sector</b></div>
-			<div class="col-md-9">{{$user->education}}</div>
-		</div>	
-		
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Leerjaar</b></div>
-			<div class="col-md-9">{{$user->school_year}}</div>
-		</div>
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Overzicht werk</b></div>
-			<div class="col-md-9">{{$user->work_summary}}</div>
-		</div>
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Kostenplaatje</b></div>
-			<div class="col-md-9">{{$user->price}}</div>
-		</div>
-		@if (Auth::check() && Auth::user()->hasOnePrivelege(['Administrator']))
-			<div class="col-md-6">
-				<div class="col-md-3"><b>Bezorg adres</b></div>
-				<div class="col-md-9">{{$user->delivery_address}}</div>
-			</div>
-			<div class="col-md-6">
-				<div class="col-md-3"><b>Postcode</b></div>
-				<div class="col-md-9">{{$user->zip}}</div>
-			</div>
-		@endif
-	</div>
 </div>
-
-<h4>Kunstwerken</h4>
-@if (count($artworks) > 0)
-	@foreach ($artworks as $artwork)
-	<div class="img-box-search">
-		<a href="/artworks/{{ $artwork->slug }}">
-			<img class="img-box-image" src="/{{$artwork->file}}" />
-			<h3>{{$artwork->title}}</h3>
-		</a>
-	</div>
-	@endforeach
-@else
-	<i>Geen</i>
-@endif
 		
 @stop
--- resources/views/users/showself.blade.php
+++ resources/views/users/showself.blade.php
@@ -5,79 +5,85 @@
 <div class="panel-heading">
 	<h1>Mijn Profiel</h1><a class="btn btn-warning profileEditButton" href="{{$user->slug}}/edit">Wijzig</a>
 </div>
-	@if ($user->profile_picture != '' || $user->biography != '')
-		<div class="container profileDetails">
-			<div class="col-md-12">
-				<div class="col-md-3"><img src="{{$user->profile_picture}}" class="profile_pic"></div>
-				<div class="col-md-9">{!! $user->biography !!}</div>
-			</div>
-		</div>
-	@endif
-	<div class="container profileDetails">
+	
+<div class="container profileDetails" style="padding: 0;">
+	
+	<div class="col-md-10">
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Naam</b></div>
-			<div class="col-md-9">{{$user->name}}</div>
+			<div class="col-md-4"><b>Naam</b></div>
+			<div class="col-md-8">{{$user->name}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Lid sinds</b></div>
-			<div class="col-md-9">{{$user->created_at}}</div>
+			<div class="col-md-4"><b>Lid sinds</b></div>
+			<div class="col-md-8">{{$user->created_at}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>E-mail</b></div>
-			<div class="col-md-9">{{$user->email}}</div>
+			<div class="col-md-4"><b>E-mail</b></div>
+			<div class="col-md-8">{{$user->email}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Telefoon nummer</b></div>
-			<div class="col-md-9">{{$user->telephone}}</div>
+			<div class="col-md-4"><b>Telefoon nummer</b></div>
+			<div class="col-md-8">{{$user->telephone}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Opleiding / Sector</b></div>
-			<div class="col-md-9">{{$user->education}}</div>
+			<div class="col-md-4"><b>Opleiding / Sector</b></div>
+			<div class="col-md-8">{{$user->education}}</div>
 		</div>	
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Leerjaar</b></div>
-			<div class="col-md-9">{{$user->school_year}}</div>
+			<div class="col-md-4"><b>Leerjaar</b></div>
+			<div class="col-md-8">{{$user->school_year}}</div>
+		</div>
+		
+		{{--<div class="col-md-6">
+			<div class="col-md-4"><b>Overzicht werk</b></div>
+			<div class="col-md-8">{{$user->work_summary}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Overzicht werk</b></div>
-			<div class="col-md-9">{{$user->work_summary}}</div>
+			<div class="col-md-4"><b>Kostenplaatje</b></div>
+			<div class="col-md-8">{{$user->price}}</div>
+		</div>--}}
+		
+		<div class="col-md-6">
+			<div class="col-md-4"><b>Bezorg adres</b></div>
+			<div class="col-md-8">{{$user->delivery_address}}</div>
 		</div>
 		
 		<div class="col-md-6">
-			<div class="col-md-3"><b>Kostenplaatje</b></div>
-			<div class="col-md-9">{{$user->price}}</div>
+			<div class="col-md-4"><b>Postcode</b></div>
+			<div class="col-md-8">{{$user->zip}}</div>
 		</div>
-		
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Bezorg adres</b></div>
-			<div class="col-md-9">{{$user->delivery_address}}</div>
-		</div>
-		
-		<div class="col-md-6">
-			<div class="col-md-3"><b>Postcode</b></div>
-			<div class="col-md-9">{{$user->zip}}</div>
+		@if ($user->biography != '')
+			<div class="col-md-12 biography">{!! $user->biography !!}</div>
+		@endif
+	</div>	
+		<div class="col-md-2">
+			@if ($user->profile_picture != '')
+				<img src="{{$user->profile_picture}}" class="profile_pic">
+			@else
+				<i class="no_profile_pic">Geen profielafbeelding</i>
+			@endif
 		</div>
 	</div>
-</div>
 
-<h4>Kunstwerken</h4>
-@if (count($artworks) > 0)
-	@foreach ($artworks as $artwork)
-	<div class="img-box-search">
-		<a href="/artworks/{{ $artwork->slug }}">
-			<img class="img-box-image" src="/{{$artwork->file}}" />
-			<h3>{{$artwork->title}}</h3>
-		</a>
-	</div>
-	@endforeach
-@else
-	<i>Geen</i>
-@endif
+	<h4>Kunstwerken</h4>
+	@if (count($artworks) > 0)
+		@foreach ($artworks as $artwork)
+		<div class="img-box-search">
+			<a href="/artworks/{{ $artwork->slug }}">
+				<img class="img-box-image" src="/{{$artwork->file}}" />
+				<h3>{{$artwork->title}}</h3>
+			</a>
+		</div>
+		@endforeach
+	@else
+		<i>Geen</i>
+	@endif
+</div>
 
 @stop
